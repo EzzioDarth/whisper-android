e05f26bd1608c9c818651d30110d091bc5857a35
EzzioDarth <adnane.debbach@gmail.com>
2025-09-24 19:43:25 -0500

    UI working login page part success build on ubuntu emulator success

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     //alias(libs.plugins.kotlin.android) apply false
     //alias(libs.plugins.kotlin.compose) apply false
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
 }
